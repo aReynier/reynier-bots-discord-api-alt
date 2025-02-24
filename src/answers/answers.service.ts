5f0ff943dc235fd026bc50fa5a848c734e6df936
Srekaens <justindidelot8@outlook.fr>
2025-02-24 14:26:33 +0100

    feat(86c21mpmv): add error handling imports
    T#86c21mpmv
    
    - Add NotFoundException import
    
    - Add BadRequestException import

--- src/answers/answers.service.ts
+++ src/answers/answers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UpdateAnswerDto } from './dto/update-answer.dto';
