0d14eeb8c223bfd6ba3734786b0be94b0d8311de
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-24 16:44:53 +0100

    fix(resources): update update resource DTO

--- src/resources/dto/update-resource.dto.ts
+++ src/resources/dto/update-resource.dto.ts
@@ -1,7 +1,7 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateResourceDto } from './create-resource.dto';
 
-// On exclut uuid_member du DTO de mise à jour
+// On exclut uuidMember du DTO de mise à jour
 export class UpdateResourceDto extends PartialType(
-  OmitType(CreateResourceDto, ['uuid_member'] as const)
+  OmitType(CreateResourceDto, ['uuidMember'] as const)
 ) {} 
