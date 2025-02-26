ee1c8f05af58889374eae0b84a596f1c0650ffaa
BborisB <b.boris.2001@gmail.com>
2025-02-26 10:39:33 +0100

    feat(86c21mx5c): enhance Question entity with API properties T#86c21mx5c

--- src/questions/entities/question.entity.ts
+++ src/questions/entities/question.entity.ts
@@ -1,23 +1,50 @@
 import { Answer } from 'src/answers/entities/answer.entity';
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToMany } from 'typeorm';
+import { Poll } from '../../polls/entities/poll.entity';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('questions')
 export class Question {
-    @PrimaryGeneratedColumn('uuid', { name: 'question_id' })
+    @ApiProperty({
+        description: 'Unique identifier of the question',
+        example: '123e4567-e89b-12d3-a456-426614174000'
+    })
+    @PrimaryGeneratedColumn('uuid', { name: 'uuid_question' })
     uuid: string;
     
+    @ApiProperty({
+        description: 'Content of the question',
+        example: 'What is your favorite color?'
+    })
     @Column()
     content: string;
 
-    @Column()
+    @ApiProperty({
+        description: 'Whether multiple answers are allowed',
+        example: true
+    })
+    @Column({name: 'is_multiple_answer'})
     isMultipleAnswer: boolean;
 
-    // @ManyToOne(() => Poll, (poll) => poll.questions)
-    // uuidPoll: Poll;
+    @ApiProperty({
+        description: 'The poll this question belongs to',
+        type: () => Poll
+    })
+    @ManyToOne(() => Poll, (poll) => poll.questions, {onDelete: 'CASCADE'})
+    @JoinColumn({ name: 'uuid_poll' })
+    poll: Poll;
 
-    @OneToMany(() => Answer, (answer) => answer.question)
-    answers: Answer[]
+    @ApiProperty({
+        description: 'The answers for this question',
+        type: () => [Answer]
+    })
+    @OneToMany(() => Answer, (answer) => answer.question, {cascade: true})
+    answers: Answer[];
 
+    @ApiProperty({
+        description: 'Creation timestamp',
+        example: '2025-02-20T10:00:00Z'
+    })
     @CreateDateColumn({
         name: 'created_at',
         type: 'timestamp',
@@ -25,6 +52,10 @@ export class Question {
     })
     createdAt: Date;
 
+    @ApiProperty({
+        description: 'Last update timestamp',
+        example: '2025-02-20T10:30:00Z'
+    })
     @UpdateDateColumn({
         name: 'updated_at',
         type: 'timestamp',
