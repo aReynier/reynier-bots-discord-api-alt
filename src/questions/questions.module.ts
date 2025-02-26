7856ccc5844a65e13c584ce0d4c85a312414ff6a
BborisB <b.boris.2001@gmail.com>
2025-02-26 10:36:20 +0100

    feat(86c21mx51): add Answer and Poll entities to Questions module T#86c21mx51

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
 import { Question } from './entities/question.entity';
+import { Answer } from 'src/answers/entities/answer.entity';
+import { Poll } from 'src/polls/entities/poll.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Question])],
+  imports: [TypeOrmModule.forFeature([Answer, Question, Poll])],
   controllers: [QuestionsController],
   providers: [QuestionsService],
   exports: [QuestionsService]
