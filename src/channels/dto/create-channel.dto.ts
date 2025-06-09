ac704fb747af1759eb15b013b07f11c01fcccea5
Aurore Reynier <reynier.aurore@gmail.com>
2025-06-09 22:42:16 +0200

    feat(86c3tpd88): enhance channel entity constraints

--- src/channels/dto/create-channel.dto.ts
+++ src/channels/dto/create-channel.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsInt, IsEnum, MaxLength, Min, Length } from 'class-validator';
+import { IsString, IsInt, IsEnum, MaxLength, Min, Length, Matches } from 'class-validator';
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { PickableDiscordIdFields} from 'src/utils/pickable-discord-id-fields';
 
@@ -19,7 +19,8 @@ export class CreateChannelDto extends PickType(PickableDiscordIdFields, [
     maxLength: 100
   })
   @IsString()
-  @MaxLength(100)
+  @Length(2, 100, { message: 'Le nom doit contenir entre 2 et 100 caractères' })
+  @Matches(/^[a-zA-ZÀ-ÿ0-9\s\-_]+$/, { message: 'Le nom ne peut contenir que des lettres (avec accents), chiffres, espaces, tirets et underscores' })
   name: string;
 
   @ApiProperty({
@@ -39,7 +40,4 @@ export class CreateChannelDto extends PickType(PickableDiscordIdFields, [
   @IsInt()
   @Min(0)
   channelPosition: number;
-
-  idGuild: string;
-  idCategory: string;
 } 
